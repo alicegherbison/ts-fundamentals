9e9f3c1fab72c069024a7207d116dc5243175446
alice <alice@ajenta.net>
2019-10-18 16:45:59 +0100

    feat: begin dictionary exercise

--- challenges/dict/src/index.ts
+++ challenges/dict/src/index.ts
@@ -1,7 +1,12 @@
-export type Dict = {};
+export type Dict<Type> = {
+  [key: string]: Type | undefined;
+};
 
 // Array.prototype.map, but for Dict
-export function mapDict() {}
+export function mapDict<Type, TransformedValues>(
+  dict: Dict<Type>,
+  fn: (arg: Type) => TransformedValues
+): Dict<TransformedValues> {}
 
 // Array.prototype.reduce, but for Dict
 export function reduceDict() {}
