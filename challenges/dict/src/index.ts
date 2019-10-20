7562fd9d0da6d9a2d4280b5f98bf6ed69cfc8b69
alicegherbison <git@alicegherbison.com>
2019-10-20 16:08:25 +0100

    feat: finish dictionary challenge

--- challenges/dict/src/index.ts
+++ challenges/dict/src/index.ts
@@ -5,8 +5,27 @@ export type Dict<Type> = {
 // Array.prototype.map, but for Dict
 export function mapDict<Type, TransformedValues>(
   dict: Dict<Type>,
-  fn: (arg: Type) => TransformedValues
-): Dict<TransformedValues> {}
+  fn: (arg: Type, index: number) => TransformedValues
+): Dict<TransformedValues> {
+  const out: Dict<TransformedValues> = {};
+
+  Object.keys(dict).forEach((dictionaryKey, index) => {
+    const thisItem = dict[dictionaryKey];
+    if (typeof thisItem !== "undefined") {
+      out[dictionaryKey] = fn(thisItem, index);
+    }
+  });
+  return out;
+}
+
+mapDict(
+  {
+    a: "a",
+    b: "b",
+    c: "c"
+  },
+  string => ({ value: string })
+);
 
 // Array.prototype.reduce, but for Dict
 export function reduceDict() {}
